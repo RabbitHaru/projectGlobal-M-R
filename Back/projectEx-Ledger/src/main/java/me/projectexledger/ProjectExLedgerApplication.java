ec42c967885efa14bd0f01236ebd82f8eb857163
RABBIT <rabbitharu1020@gmail.com>
2026-02-24 12:37:40 +0900

    2026-02-24 Back, Front 기본 프로젝트 파일

---
+++ Back/projectEx-Ledger/src/main/java/me/projectexledger/ProjectExLedgerApplication.java
@@ -0,0 +1,13 @@
+package me.projectexledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectExLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectExLedgerApplication.class, args);
+    }
+
+}
---
+++ Back/projectEx-Ledger/src/test/java/me/projectexledger/ProjectExLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.projectexledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectExLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
