7dca9c076b94906e879666de856f496dea9d74ed
RABBIT <rabbitharu1020@gmail.com>
2026-02-27 14:03:27 +0900

    2026-02-27 application.properties 수정

--- Back/projectEx-Ledger/src/main/java/me/projectexledger/ProjectExLedgerApplication.java
+++ Back/projectEx-Ledger/src/main/java/me/projectexledger/ProjectExLedgerApplication.java
@@ -2,11 +2,9 @@ package me.projectexledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling; // 1. 임포트 추가
 
 @EnableScheduling
-@EnableJpaAuditing
 @SpringBootApplication
 public class ProjectExLedgerApplication {
 
